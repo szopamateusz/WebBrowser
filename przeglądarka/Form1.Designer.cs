7e1faf2bc6cb14eb4d030432ca6ee60495be1e4d
szopamateusz <github@mateusz-szopa.pl>
2017-02-26 14:54:28 +0100

    Add files via upload

---
+++ przeglądarka/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace przeglądarka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowaKartaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.noweOknoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informacjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zamknijToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.toolStrip1);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1009, 78);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(249, 44);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(79, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Odśwież";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(166, 44);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(77, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(78, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Przód";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Wstecz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(334, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Przejdź";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(729, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 650);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1054, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(64, 17);
+            this.toolStripStatusLabel1.Text = "Ładowanie";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1054, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowaKartaToolStripMenuItem,
+            this.noweOknoToolStripMenuItem,
+            this.informacjeToolStripMenuItem,
+            this.zamknijToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(50, 20);
+            this.toolStripMenuItem1.Text = "Menu";
+            // 
+            // nowaKartaToolStripMenuItem
+            // 
+            this.nowaKartaToolStripMenuItem.Name = "nowaKartaToolStripMenuItem";
+            this.nowaKartaToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.nowaKartaToolStripMenuItem.Text = "Nowa karta";
+            this.nowaKartaToolStripMenuItem.Click += new System.EventHandler(this.nowaKartaToolStripMenuItem_Click);
+            // 
+            // noweOknoToolStripMenuItem
+            // 
+            this.noweOknoToolStripMenuItem.Name = "noweOknoToolStripMenuItem";
+            this.noweOknoToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.noweOknoToolStripMenuItem.Text = "Nowe okno";
+            this.noweOknoToolStripMenuItem.Click += new System.EventHandler(this.noweOknoToolStripMenuItem_Click);
+            // 
+            // informacjeToolStripMenuItem
+            // 
+            this.informacjeToolStripMenuItem.Name = "informacjeToolStripMenuItem";
+            this.informacjeToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.informacjeToolStripMenuItem.Text = "Informacje";
+            this.informacjeToolStripMenuItem.Click += new System.EventHandler(this.informacjeToolStripMenuItem_Click);
+            // 
+            // zamknijToolStripMenuItem
+            // 
+            this.zamknijToolStripMenuItem.Name = "zamknijToolStripMenuItem";
+            this.zamknijToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.zamknijToolStripMenuItem.Text = "Zamknij";
+            this.zamknijToolStripMenuItem.Click += new System.EventHandler(this.zamknijToolStripMenuItem_Click);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Location = new System.Drawing.Point(0, 111);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(1054, 536);
+            this.tabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl.TabIndex = 4;
+            this.tabControl.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.tabControl_MouseDoubleClick);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.CanOverflow = false;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(12, 16);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(656, 25);
+            this.toolStrip1.TabIndex = 14;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(600, 25);
+            this.toolStripComboBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.toolStripComboBox1_KeyPress);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(673, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Google";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 672);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "WWW";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem nowaKartaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem noweOknoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informacjeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zamknijToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ przeglądarka/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace przeglądarka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int nr = 0;
+        WebBrowser webBrowser = new WebBrowser();
+
+        public void GoToSite()
+        {
+            try
+            {
+                if (toolStripComboBox1.Text != "")
+                {
+                    if (!toolStripComboBox1.Text.StartsWith("http://") &&
+                        !toolStripComboBox1.Text.StartsWith("https://"))
+                    {
+                        toolStripComboBox1.Text = @"http://" + toolStripComboBox1.Text;
+                        ((WebBrowser) tabControl.SelectedTab.Controls[0]).Navigate(toolStripComboBox1.Text);
+                        if (!toolStripComboBox1.Items.Contains(toolStripComboBox1.Text))
+                        {
+                            toolStripComboBox1.Items.Add(toolStripComboBox1.Text);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show(@"Niespodziewany błąd. Przepraszamy");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser = new WebBrowser();
+            webBrowser.ScriptErrorsSuppressed = true;
+            webBrowser.Dock = DockStyle.Fill;
+            webBrowser.Visible = true;
+            webBrowser.DocumentCompleted += webBrowser_DocumentCompleted;
+            tabControl.TabPages.Add("Nowa karta");
+            tabControl.SelectTab(nr);
+            tabControl.SelectedTab.Controls.Add(webBrowser);
+            ((WebBrowser) tabControl.SelectedTab.Controls[0]).Navigate("google.pl");
+            nr += 1;
+        }
+
+        void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            try
+            {
+                tabControl.SelectedTab.Text = ((WebBrowser) tabControl.SelectedTab.Controls[0]).DocumentTitle;
+                toolStripComboBox1.Text = ((WebBrowser) tabControl.SelectedTab.Controls[0]).Url.ToString();
+                if (((WebBrowser) tabControl.SelectedTab.Controls[0]).CanGoBack) button2.Enabled = true;
+                else button2.Enabled = false;
+                if (((WebBrowser) tabControl.SelectedTab.Controls[0]).CanGoForward) button3.Enabled = true;
+                else button3.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show(@"Niespodziewany błąd. Przepraszamy");
+            }
+        }
+
+        private void zamknijToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                GoToSite();
+            }
+            catch
+            {
+                MessageBox.Show(@"Niespodziewany błąd. Przepraszamy");
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char) 13)
+            {
+                try
+                {
+                    string url = $"http://www.google.pl/search?q={textBox1.Text.Replace(" ", "+")}";
+                    ((WebBrowser) tabControl.SelectedTab.Controls[0]).Navigate(url);
+                }
+                catch
+                {
+                    MessageBox.Show(@"Niespodziewany błąd. Przepraszamy");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser) tabControl.SelectedTab.Controls[0]).GoBack();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser) tabControl.SelectedTab.Controls[0]).GoForward();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser) tabControl.SelectedTab.Controls[0]).Stop();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser) tabControl.SelectedTab.Controls[0]).Refresh();
+        }
+
+        private void noweOknoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 f = new Form1();
+            f.Show();
+        }
+
+        private void informacjeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"Mateusz Szopa \n IVIA \n 2014");
+        }
+
+
+        private void nowaKartaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            webBrowser = new WebBrowser();
+            webBrowser.ScriptErrorsSuppressed = true;
+            webBrowser.Dock = DockStyle.Fill;
+            webBrowser.Visible = true;
+            webBrowser.DocumentCompleted += webBrowser_DocumentCompleted;
+            tabControl.TabPages.Add("Nowa karta");
+            tabControl.SelectTab(nr);
+            tabControl.SelectedTab.Controls.Add(webBrowser);
+            nr += 1;
+            ((WebBrowser) tabControl.SelectedTab.Controls[0]).Navigate("google.pl");
+        }
+
+        private void tabControl_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+             
+                if (tabControl.TabPages.Count - 1 > 0)
+                {
+                    tabControl.TabPages.RemoveAt(tabControl.SelectedIndex);
+                    tabControl.SelectTab(tabControl.TabPages.Count - 1);
+                    nr -= 1;
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+            catch
+            {
+                MessageBox.Show(@"Niespodziewany błąd. Przepraszamy");
+            }
+        }
+
+        private void tabControl_TabIndexChanged(object sender, EventArgs e)
+        {
+            toolStripComboBox1.Text = ((WebBrowser) tabControl.SelectedTab.Controls[0]).Url.ToString();
+        }
+
+        private void tabControl_TabStopChanged(object sender, EventArgs e)
+        {
+            toolStripComboBox1.Text = ((WebBrowser) tabControl.SelectedTab.Controls[0]).Url.ToString();
+        }
+
+        private void toolStripComboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char) 13)
+            {
+                try
+                {
+                    GoToSite();
+                }
+                catch
+                {
+                    MessageBox.Show(@"Niespodziewany błąd. Przepraszamy");
+                }
+            }
+        }
+
+        private void toolStripComboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void toolStripComboBox1_MouseEnter(object sender, EventArgs e)
+        {
+            ((WebBrowser) tabControl.SelectedTab.Controls[0]).Navigate(toolStripComboBox1.Text);
+        }
+    }
+}
